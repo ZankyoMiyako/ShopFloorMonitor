35b72e0c6f46b4b00b9715715075fe27c1a963dc
ZankyoMiyako <ZankyoMiyako@outlook.com>
2026-03-10 20:53:57 +0800

    feat:做了简单了导航栏

--- 04_Shell/ViewModels/MainWindowViewModel.cs
+++ 04_Shell/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _04_Shell.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : BindableBase
     {
+        public MainWindowViewModel()
+        {
+            CreateMenuBars();
+        }
+
+        public class MenuBar
+        {
+            public string Icon { get; set; }
+            public string ViewName { get; set; }
+        }
+        private ObservableCollection<MenuBar> _menuBars;
+
+        public ObservableCollection<MenuBar> MenuBars
+        {
+            get { return _menuBars; }
+            set
+            {
+                _menuBars = value;
+                RaisePropertyChanged();
+            }
+        }
+        private void CreateMenuBars()
+        {
+            MenuBars= new ObservableCollection<MenuBar>();
+            MenuBars.Add(new MenuBar { Icon = "HomeOutline", ViewName = "1" });
+            MenuBars.Add(new MenuBar { Icon = "HomeOutline", ViewName = "1" });
+            MenuBars.Add(new MenuBar { Icon = "HomeOutline", ViewName = "1" });
+            MenuBars.Add(new MenuBar { Icon = "HomeOutline", ViewName = "1" });
+            MenuBars.Add(new MenuBar { Icon = "HomeOutline", ViewName = "1" });
+            MenuBars.Add(new MenuBar { Icon = "HomeOutline", ViewName = "1" });
+        }
+
     }
 }
