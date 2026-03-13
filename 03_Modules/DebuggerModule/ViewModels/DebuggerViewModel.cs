975357eb6ff9b4b748087f4446970d731811abae
ZankyoMiyako <ZankyoMiyako@outlook.com>
2026-03-13 16:00:32 +0800

    feat:添加了配置TCP与RTU连接参数的视图,并实现了侧边栏打开后加载两者

--- 03_Modules/DebuggerModule/ViewModels/DebuggerViewModel.cs
+++ 03_Modules/DebuggerModule/ViewModels/DebuggerViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _03_Modules.DebuggerModule.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,64 @@ using System.Threading.Tasks;
 
 namespace _03_Modules.DebuggerModule.ViewModels
 {
-    public class DebuggerViewModel
+    public class DebuggerViewModel : BindableBase
     {
+        public DebuggerViewModel()
+        {
+            DrawerControl = new DelegateCommand<string>(OpenDrawer);
+        }
+        #region 侧边栏开关and内容
+        private bool _isRightDrawerOpen;
+        public bool IsRightDrawerOpen
+        {
+            get { return _isRightDrawerOpen; }
+            set
+            {
+                SetProperty(ref _isRightDrawerOpen, value);
+            }
+        }
+        private object _rightDrawerContent;
+
+        public object RightDrawerContent
+        {
+            get { return _rightDrawerContent; }
+            set
+            {
+                SetProperty(ref _rightDrawerContent, value);
+            }
+        }
+
+        private TCPConnectView _cachedTCPConnectView;
+        private RTUConnectView _cachedRTUConnectView;
+        #endregion
+        public DelegateCommand<string> DrawerControl { get; set; }
+        private void OpenDrawer(string obj)
+        {
+            IsRightDrawerOpen = true;
+            switch (obj)
+            {
+                case "TCP": LoadTCPConnectView(); break;
+                case "RTU": LoadRTUConnectView(); break;
+            }
+        }
+
+        private void LoadRTUConnectView()
+        {
+            if (_cachedRTUConnectView == null)
+            {
+                _cachedRTUConnectView = new RTUConnectView();
+            }
+            RightDrawerContent = _cachedRTUConnectView;
+        }
+
+        private void LoadTCPConnectView()
+        {
+            if (_cachedTCPConnectView == null)
+            {
+                _cachedTCPConnectView = new TCPConnectView();
+            }
+            RightDrawerContent = _cachedTCPConnectView;
+        }
+
     }
 }
---
+++ 03_Modules/DebuggerModule/ViewModels/TCPConnectViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Modules.DebuggerModule.ViewModels
+{
+    internal class TCPConnectViewModel:BindableBase
+    {
+        public TCPConnectViewModel()
+        {
+            Debug.WriteLine("启动成功");
+        }
+    }
+}
