2ae5999db06288e36f8c4f26fde015d34c9b759a
ZankyoMiyako <ZankyoMiyako@outlook.com>
2026-03-15 16:20:01 +0800

    feat:编写了RTU参数总选项,并在对应视图中完成数据绑定,同时通过编写事件实现了Debugger与RTU视图的连接参数互通与保存修改

---
+++ 01_Core/Models/RTUConnectOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Core.Models
+{
+    public class RTUConnectOptions
+    {
+        public ObservableCollection<string> SerialPortOptions { get; }
+        public ObservableCollection<int> BaudRateOptions { get; } = new(){ 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200 };
+        public ObservableCollection<int> DataBitOptions { get; } = new() { 5,6,7,8};
+        public ObservableCollection<StopBits> StopBitOptions { get; } = new() {StopBits.None,StopBits.One,StopBits.Two,StopBits.OnePointFive };
+        public ObservableCollection<Parity> ParityBitOptions { get; } = new() {Parity.None,Parity.Odd,Parity.Even,Parity.Mark,Parity.Space };
+
+        public RTUConnectOptions()
+        {
+            var prots = SerialPort.GetPortNames();
+            SerialPortOptions = prots.Length > 0 ? new ObservableCollection<string>(prots) : new ObservableCollection<string> { "COM3" };
+        }
+    }
+}
--- 03_Modules/DebuggerModule/ViewModels/RTUConnectViewModel.cs
+++ 03_Modules/DebuggerModule/ViewModels/RTUConnectViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using _01_Core.Events;
+using _01_Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,20 +8,40 @@ using System.Threading.Tasks;
 
 namespace _03_Modules.DebuggerModule.ViewModels
 {
-    internal class RTUConnectViewModel
+    internal class RTUConnectViewModel:BindableBase
     {
         private IEventAggregator _eventAggregator;
         public RTUConnectViewModel(IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
+            RTUConnectOptions=new RTUConnectOptions();
             DrawerControl = new DelegateCommand<string>(CloseDrawer);
+            _eventAggregator.GetEvent<ModbusConnectParamsRequestEvent>().Subscribe(args =>
+            {
+                ConnectParams = args;
+            },ThreadOption.UIThread);
         }
 
         private void CloseDrawer(string obj)
         {
             _eventAggregator.GetEvent<DrawerControlEvent>().Publish(false);
+            if (obj == "确定")
+            {
+                _eventAggregator.GetEvent<ModbusConnectParamsUpdateEvent>().Publish(ConnectParams);
+            }
         }
 
         public DelegateCommand<string> DrawerControl { get; set; }
+
+        private ModbusConnectParams _connectParams;
+        public ModbusConnectParams ConnectParams
+        {
+            get { return _connectParams; }
+            set
+            {
+                SetProperty(ref _connectParams, value);
+            }
+        }
+        public RTUConnectOptions RTUConnectOptions { get; set; }
     }
 }
