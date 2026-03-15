cbd4b1e43d4b8f95bc16ad0502f2fe4aaa376a06
ZankyoMiyako <ZankyoMiyako@outlook.com>
2026-03-15 16:15:55 +0800

    rename:重命名重连次数字段名称:RetryTime => RetryCount

--- 01_Core/Models/ModbusConnectParams.cs
+++ 01_Core/Models/ModbusConnectParams.cs
@@ -13,16 +13,16 @@ namespace _01_Core.Models
         //===================公共属性=====================
         public ModbusConnectType ModbusConnectType { get; set; } = ModbusConnectType.TCP;
         public int TimeOut { get; set; } = 2000;
-        public int RetryTime { get; set; } = 3;
+        public int RetryCount { get; set; } = 3;
 
         //===============TCP特有属性=======================
         public string IpAdress { get; set; } = "127.0.0.1";
         public int Port { get; set; } = 502;
 
         //===============RTU特有属性=========================
-        public string SerialPort { get; set; } = "COM1";
+        public string SerialPort { get; set; } = "COM3";
         public int BaudRate { get; set; } = 9600;
-        public int DataBits { get; set; } = 8;
+        public int DataBit { get; set; } = 8;
         public StopBits StopBit { get; set; }=StopBits.One;
         public Parity ParityBit { get; set; } = Parity.None;
     }
